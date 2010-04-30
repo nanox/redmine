nanox/redmine

commit bed8bb9b5231899e97d33cc2dca1923fc3dafd8f

Fixes acts_as_event with a String as url option (#5343).
git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@3681 e93f8b46-1217-0410-a6f0-8f06a7374b81

vendor/plugins/acts_as_event/lib/acts_as_event.rb
@@ -65,7 +65,15 @@ module Redmine
         
         def event_url(options = {})
           option = event_options[:url]
-          (option.is_a?(Proc) ? option.call(self) : send(option)).merge(options)
+          if option.is_a?(Proc)
+            option.call(self)
+          elsif option.is_a?(Hash)
+            option.merge(options)
+          elsif option.is_a?(Symbol)
+            send(option)
+          else
+            option
+          end
         end
 
         # Returns the mail adresses of users that should be notified

commit 0e1be8c187c58590e69bb0947489cb7b471f7686

Fixes URLs in atom feeds broken by r3681 (#5403).

vendor/plugins/acts_as_event/lib/acts_as_event.rb
@@ -66,11 +66,11 @@ module Redmine
         def event_url(options = {})
           option = event_options[:url]
           if option.is_a?(Proc)
-            option.call(self)
+            option.call(self).merge(options)
           elsif option.is_a?(Hash)
             option.merge(options)
           elsif option.is_a?(Symbol)
-            send(option)
+            send(option).merge(options)
           else
             option
           end

test/functional/projects_controller_test.rb
@@ -447,6 +447,9 @@ class ProjectsControllerTest < ActionController::TestCase
     get :activity, :format => 'atom'
     assert_response :success
     assert_template 'common/feed.atom.rxml'
+    assert_tag :tag => 'entry', :child => {
+      :tag => 'link',
+      :attributes => {:href => 'http://test.host/issues/11'}}
   end
   
   def test_archive