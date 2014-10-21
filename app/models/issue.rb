nanox/redmine

commit 51508f67d0cc716d665d8a32ac3bffdf7af44ea4

Fixed test failure with SQLServer introduced by r13461.
git-svn-id: http://svn.redmine.org/redmine/trunk@13467 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/models/issue.rb
@@ -1229,9 +1229,10 @@ class Issue < ActiveRecord::Base
       count.
       map do |columns, total|
         status_id, is_closed, field_value = columns
+        is_closed = ['t', 'true', true].include?(is_closed)
         {
           "status_id" => status_id.to_s,
-          "closed" => is_closed.to_s,
+          "closed" => is_closed,
           select_field => field_value.to_s,
           "total" => total.to_s
         }

commit 0a5529b6eaa3dbd314b4fdc911f77f34d00ed645

Fixed test failure with MySQL introduced by r13467.

app/models/issue.rb
@@ -1229,7 +1229,7 @@ class Issue < ActiveRecord::Base
       count.
       map do |columns, total|
         status_id, is_closed, field_value = columns
-        is_closed = ['t', 'true', true].include?(is_closed)
+        is_closed = ['t', 'true', '1'].include?(is_closed.to_s)
         {
           "status_id" => status_id.to_s,
           "closed" => is_closed,